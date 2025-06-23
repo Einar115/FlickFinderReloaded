37744d97d1f9556b0de7250971443759dfb3042c
Einar115 <einarnaim@hotmail.com>
2025-06-22 21:00:59 -0600

    estructura inicial

---
+++ backend/content-service/src/test/java/mx/primis/content_service/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.primis.content_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
